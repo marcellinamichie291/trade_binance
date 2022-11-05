71ddd94574f9ae0baa4992d053ab80db10b807f2
icarussfalls <109421980+icarussfalls@users.noreply.github.com>
2022-11-05 10:45:39 +0545

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -115,7 +115,7 @@ def trun_n_d(n,d):
 
 
 def strategy(pair, qty):
-    tframe = '5m' #for timeout
+    tframe = '1h' #for timeout
     #make the bot sleep if the tp or sl hits 
     time.sleep(600) #for 10 mins
     df = gethourlydata('BTCUSDT', '1h', '3days')
